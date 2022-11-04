7a95efb94173e8d59b0b4d60c30e88ffe993926f
mikun <timilehinonifara@gmail.com>
2022-11-04 13:53:48 -0700

    updated

--- src/Components/SwingSection/SwingVideos/Videos.jsx
+++ src/Components/SwingSection/SwingVideos/Videos.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Videos.module.css";
 
 const Videos = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className={classes.main}>
       <div className={classes.vidContainer}>
@@ -51,7 +53,77 @@ const Videos = () => {
           <p>View 2</p>
         </div>
       </div>
-      <button>More Swings</button>
+      {show ? (
+        <>
+          <div className={classes.swingPee}>
+            <p> Driver - Full swing - 234 Yards - DTL view</p>
+          </div>
+          <div className={classes.vidContainerTwo}>
+            <div className={classes.vidContent}>
+              <iframe
+                src="https://www.youtube.com/embed/ERIoYC4bBxA"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <p>View 1</p>
+            </div>
+            <div className={classes.vidContent}>
+              <iframe
+                src="https://www.youtube.com/embed/6R0zYAv5OFY"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <p>View 2</p>
+            </div>
+          </div>
+          <div className={classes.swingPee}>
+            <p> Driver - Full swing - 224 Yards - Fv view</p>
+          </div>
+          <div className={classes.vidContainerTwo}>
+            <div className={classes.veedContent}>
+              <iframe
+                src="https://www.youtube.com/embed/r9wEeQUFg-I"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <p>Fv View</p>
+            </div>
+          </div>
+          <div className={classes.swingPee}>
+            <p> Driver - Full swing - 184 Yards - Fv & DTL view</p>
+          </div>
+          <div className={classes.vidContainerTwo}>
+            <div className={classes.vidContent}>
+              <iframe
+                src="https://www.youtube.com/embed/4D7O9yVNeRs"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <p>Fv View</p>
+            </div>
+            <div className={classes.vidContent}>
+              <iframe
+                src="https://www.youtube.com/embed/UyOT5-IkhxA"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <p>DTL View</p>
+            </div>
+          </div>
+        </>
+      ) : null}
+      <button onClick={() => setShow(!show)}>More Swings</button>
+      {/* <button onClick={() => setShow(false)}>Less Swings</button> */}
     </div>
   );
 };
