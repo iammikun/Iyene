69a61f6f25201972bd9e789f05803471cd9454eb
mikun <timilehinonifara@gmail.com>
2022-11-21 11:58:19 -0800

    final adj

--- src/Components/MobileNav/MobileNav.jsx
+++ src/Components/MobileNav/MobileNav.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./MobileNav.module.css";
 import { CgMenuLeft } from "react-icons/cg";
+import { AiOutlineDown } from "react-icons/ai";
 import { CgClose } from "react-icons/cg";
-import NavCompo from "./NavCompo";
 
 const MobileNav = () => {
   const [open, setOpen] = useState(false);
@@ -28,7 +28,13 @@ const MobileNav = () => {
 
   const closeMenu = () => {
     setOpen(false);
+    setDropDown(false);
+    setDropDownOff(false);
   };
+
+  const [dropDown, setDropDown] = useState(false);
+
+  const [dropDownOff, setDropDownOff] = useState(false);
 
   return (
     <div className={classes.main}>
@@ -36,7 +42,6 @@ const MobileNav = () => {
         <div className={classes.mobileView}>
           <Link to="/" className={classes.logo} onClick={closeMenu}>
             <h1>Iyene Essien</h1>
-            {/* <img src={logoDark} alt="" /> */}
           </Link>
         </div>
       </div>
@@ -52,7 +57,55 @@ const MobileNav = () => {
           <Link to="swing" onClick={closeMenu}>
             <li>Swing Videos</li>
           </Link>
-          <NavCompo />
+          <div className={classes.navComponent}>
+            <div className={dropDown ? classes.dropDown : classes.onCourse}>
+              <span>
+                ON COURSE{" "}
+                <AiOutlineDown
+                  className={classes.arrow}
+                  onClick={() => setDropDown(!dropDown)}
+                />
+              </span>
+              <div className={classes.hover}>
+                <Link to="gallery" onClick={closeMenu}>
+                  gallery
+                </Link>
+                <Link to="schedule" onClick={closeMenu}>
+                  Schedule
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className={classes.offComponent}>
+            <div
+              className={dropDownOff ? classes.dropDownOff : classes.offCourse}
+            >
+              <span>
+                OFF COURSE{" "}
+                <AiOutlineDown
+                  className={classes.arrow}
+                  onClick={() => setDropDownOff(!dropDownOff)}
+                />
+              </span>
+              <div className={classes.hover}>
+                <Link to="news" onClick={closeMenu}>
+                  News
+                </Link>
+                <Link to="interview" onClick={closeMenu}>
+                  Interviews
+                </Link>
+                <Link to="community-service" onClick={closeMenu}>
+                  Community services
+                </Link>
+                <Link to="school-records" onClick={closeMenu}>
+                  Academics
+                </Link>
+                <Link to="awards" onClick={closeMenu}>
+                  Awards
+                </Link>
+              </div>
+            </div>
+          </div>
           <Link to="contact" onClick={closeMenu}>
             <li>Contact</li>
           </Link>
