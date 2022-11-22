12daca29db86ef3723d5cfa5c1e016bea5744ef7
mikun <timilehinonifara@gmail.com>
2022-11-22 10:20:32 -0800

    finale

--- src/Components/NewsSection/NewsService/NewsService.jsx
+++ src/Components/NewsSection/NewsService/NewsService.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./NewsService.module.css";
 import action from "../../../Asset/Image/action.jpeg";
 import malala from "../../../Asset/Image/malala.jpeg";
+import community from "../../../Asset/Image/community.jpg";
 
 const NewsService = () => {
   return (
@@ -55,7 +56,7 @@ const NewsService = () => {
             I was one of the three golfers selected in the world to feature in
             the series and featured in the campaign:{" "}
             <a href="https://assembly.malala.org/stories/female-golfers-taking-a-swing-at-the-establishment">
-              Check Out More Of This
+              SEE MORE
             </a>
           </p>
           <p>
@@ -69,6 +70,40 @@ const NewsService = () => {
           <a href="https://assembly.malala.org/stories/female-golfers-taking-a-swing-at-the-establishment">
             SEE MORE
           </a>
+        </div>
+      </div>
+      <div className={classes.line}></div>
+      <div className={classes.mainTwo}>
+        <div className={classes.serviceContainerTwo}>
+          <h2>
+            Iyene's passion has always been focused on giving back to the
+            society.
+          </h2>
+          <p>
+            As part of Iyene’s passion and effort in giving back to the society,
+            she was invited to participate and play in a Pro/Am golfing charity
+            event - East Coast Classic - organized by Betsy King’s Golf Fore
+            Africa. Iyene received many revs and appreciation for her
+            participation and performance at the event.
+          </p>
+          <p>
+            The event was played at Siwanoy Country Club, Bronxville,
+            Eastchester, New York, June 13, 2022. The event had many active LPGA
+            professional golfers in attendance including the female golf legend
+            Laura Davis. Iyene Essien was the only junior golfer. Iyene shot
+            2-under par 72 and won the longest drive.
+          </p>
+          <p>
+            Golf Fore Africa is a non-for-profit organization created by Betsy
+            King, former LPGA professional, with a mission « To bring hope and
+            change to children & families living in extreme poverty in Africa by
+            empowering the golf community to support transformational programs
+            that holistically enhance the families we serve » through golf.
+          </p>
+          <a href="https://golfforeafrica.org/">SEE MORE</a>
+        </div>
+        <div className={classes.serviceImg}>
+          <img src={community} alt="" />
         </div>
       </div>
       <div className={classes.line}></div>
